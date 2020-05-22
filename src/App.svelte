d9b1e9a222ba65e5cc8e785f1ef1bb2666804e12
Leonis Ocampo Nieves <leonis.ocampo@outlook.com>
2020-05-22 10:05:50 -0500

    Proyecto terminado

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import MovieItem from './Movie/Item.svelte'
+	import { fly } from 'svelte/transition';
 
 	const APIKEY = '29ed1d64cc3508c30f08131eb1860d99';
 	const BASEURL = 'https://api.themoviedb.org/3';
@@ -13,6 +14,29 @@
 
 		return await response.json();
 	})()
+
+	let likedMovies = [];
+
+	function toggleLike(event) {
+		const movie = event.detail;
+
+		let index = likedMovies.findIndex(m => m.id === movie.id);
+
+		if(index >= 0) {
+			likedMovies.splice(index, 1);
+		} else {
+			likedMovies.push(movie);
+		}
+
+		likedMovies = likedMovies;
+	}
+
+	// Comprobamos que la pelicula, sea o no, agregada a favoritos
+	$: like = (id) => {
+		let index = likedMovies.findIndex(m => m.id === id);
+
+		return index >= 0;
+	}
 </script>
 
 <svelte:head>
@@ -23,24 +47,55 @@
 
 <main class="container">
 	<div class="row">
-		{#await movies}
-			<p>Cargando datos...</p>
-		{:then data}
-			{@debug data}
+		<div class="col-12 col-md-6 col-lg-8 border panel">
+			<h1>Peliculas populares</h1>
 
-			{#each data.results as movie}
-				<div class="col-12 col-md-6 col-lg-3 p-2">
-					<MovieItem id={movie.id} title={movie.title} overview={movie.overview} cover={movie.poster_path} />
-				</div>
-			{/each}
-		{/await}
+			<div class="row">
+				{#await movies}
+					<div class="col-12">
+						<p>Cargando peliculas...</p>
+					</div>
+				{:then data}
+					{@debug data}
+
+					{#each data.results as movie}
+						<div class="col-12 col-md-6 col-lg-4 p-2">
+							<MovieItem like={like(movie.id)} id={movie.id} title={movie.title} overview={movie.overview} cover={movie.poster_path} on:onToggleLike={toggleLike} />
+						</div>
+					{/each}
+				{/await}
+			</div>
+		</div>
+
+		<div class="col-12 col-md-6 col-lg-4 border panel">
+			<h2>Peliculas favoritas</h2>
+
+			<div class="row">
+				{#if likedMovies.length}
+					{#each likedMovies as movie, i (movie.id)}
+						<div in:fly="{{duration: 200, y: 20}}" out:fly="{{duration: 800, y: -20}}" class="col-12 col-md-6 col-lg-4 p-2">
+							<MovieItem like={like(movie.id)} id={movie.id} title={movie.title} overview="" cover={movie.cover} on:onToggleLike={toggleLike} />
+						</div>
+					{/each}
+				{:else}
+					<div class="col-12">
+						<p>No tienes peliculas favoritas</p>
+					</div>
+				{/if}
+			</div>
+		</div>
 	</div>
 </main>
 
 <style>
+	.panel {
+		height: 100vh;
+		overflow: auto;
+	}
+
 	main {
 		text-align: center;
-		padding: 1em;
+		padding: 0;
 		max-width: 240px;
 		margin: 0 auto;
 	}
--- src/Movie/Item.svelte
+++ src/Movie/Item.svelte
@@ -1,10 +1,19 @@
 <script>
+    import { createEventDispatcher } from 'svelte';
+
     export let id;
     export let title;
     export let overview;
     export let cover;
+    export let like;
 
     $: coverURL = `https://image.tmdb.org/t/p/w185_and_h278_bestv2${cover}`;
+
+    const dispatch = createEventDispatcher();
+    
+    function toggleLike() {
+        dispatch('onToggleLike', {id, title, overview, cover})
+    }
 </script>
 
 <div class="card">
@@ -15,7 +24,7 @@
         <p class="card-text">{overview}</p>
         
         <div class="text-center">
-            <button class="btn btn-primary">Like</button>
+            <button class={like ? 'btn btn-secondary' : 'btn btn-primary'} on:click={toggleLike}>{like ? 'Ya no me gusta' : 'Me gusta'}</button>
         </div>
     </div>
 </div>
