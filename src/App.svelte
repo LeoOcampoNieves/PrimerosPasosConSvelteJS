a4e5ea4e4759d2fcdf54b2975085100db7b82f7b
Leonis Ocampo Nieves <leonis.ocampo@outlook.com>
2020-05-22 09:06:57 -0500

    Consumiendo API movies

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,40 @@
 <script>
-	export let name;
+	import MovieItem from './Movie/Item.svelte'
+
+	const APIKEY = '29ed1d64cc3508c30f08131eb1860d99';
+	const BASEURL = 'https://api.themoviedb.org/3';
+	const APISETTINGS = `?api_key=${APIKEY}&language=es-MX`;
+
+	//Obtengo las peliculas
+	const movies = (async () => {
+		const URL = `${BASEURL}/discover/movie${APISETTINGS}$sort_by=popularity.desc`;
+		
+		const response = await fetch(URL);
+
+		return await response.json();
+	})()
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<svelte:head>
+	<title>Movies SvelteJS</title>
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</svelte:head>
+
+<main class="container">
+	<div class="row">
+		{#await movies}
+			<p>Cargando datos...</p>
+		{:then data}
+			{@debug data}
+
+			{#each data.results as movie}
+				<div class="col-12 col-md-6 col-lg-3 p-2">
+					<MovieItem id={movie.id} title={movie.title} overview={movie.overview} cover={movie.poster_path} />
+				</div>
+			{/each}
+		{/await}
+	</div>
 </main>
 
 <style>
@@ -13,13 +43,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
---
+++ src/Movie/Item.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let id;
+    export let title;
+    export let overview;
+    export let cover;
+
+    $: coverURL = `https://image.tmdb.org/t/p/w185_and_h278_bestv2${cover}`;
+</script>
+
+<div class="card">
+    <img class="card-img-top" src={coverURL} alt="Card image cap">
+    
+    <div class="card-body">
+        <h5 class="card-title">{title}</h5>
+        <p class="card-text">{overview}</p>
+        
+        <div class="text-center">
+            <button class="btn btn-primary">Like</button>
+        </div>
+    </div>
+</div>
